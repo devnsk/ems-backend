3ed48ebad9c6e5e965db0da50e07af946ce66974
Mukteswar Tripathy <mukteswart5@gmail.com>
2025-03-10 00:41:56 +0530

    Created a product repository interface

--- src/main/java/org/emp/ems_backend/product/entity/Product.java
+++ src/main/java/org/emp/ems_backend/product/entity/Product.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="products")
+@Table(name="product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
