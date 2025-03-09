ece2224f15cc380599f537c0782f56f556236a1b
Mukteswar Tripathy <mukteswart5@gmail.com>
2025-03-10 00:19:48 +0530

    Created a new entity class for product.

---
+++ src/main/java/org/emp/ems_backend/product/entity/Product.java
@@ -0,0 +1,34 @@
+package org.emp.ems_backend.product.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(name = "prod_name")
+    private String productName;
+
+    @Column(name = "prod_category")
+    private String productCategory;
+
+    @Column(name = "prod_price")
+    private Double productPrice;
+
+    @Column(name = "prod_description")
+    private String productDescription;
+
+    @Column(name = "project_id", nullable = false, unique = true)
+    private Long projectId;
+}
